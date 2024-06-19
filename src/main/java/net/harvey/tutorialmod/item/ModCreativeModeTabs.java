562f189741303cafd147bd08ec4f29a1214c0193
FlameWing-Code <hhuls604@gmail.com>
2024-06-18 23:39:11 -0400

    I LOVE ADDING STUFF

--- src/main/java/net/harvey/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/harvey/tutorialmod/item/ModCreativeModeTabs.java
@@ -36,6 +36,10 @@ public class ModCreativeModeTabs {
 
                         pOutput.accept(ModBlocks.SOUND_BLOCK.get());
 
+                        pOutput.accept(ModItems.STRAWBERRY.get());
+
+                        pOutput.accept(ModItems.PINE_CONE.get());
+
                     })
                     .build());
 
