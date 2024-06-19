1d73c2b925ee46463ec30c99c6ee8f4daa552a32
FlameWing-Code <hhuls604@gmail.com>
2024-06-19 01:19:59 -0400

    I LOVE ADDING STUFF

--- src/main/java/net/harvey/tutorialmod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/harvey/tutorialmod/datagen/ModBlockStateProvider.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
-        super(output, TutorialMod.MOD_ID, exFileHelper;
+        super(output, TutorialMod.MOD_ID, exFileHelper);
     }
 
     @Override
